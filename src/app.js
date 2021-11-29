e125af9d6724a05a301f32ef5e46da316e7e27b1
JoseIsra <israelmyname@gmail.com>
2021-11-28 20:47:23 -0500

    cambiando puerto

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const routerApi = require("./routes/index");
 const {
